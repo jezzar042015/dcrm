d44af7f448618b129f527c2f92ca7cf2f04696b4
Jezreel Tan <jezdtanslc90@gmail.com>
2026-06-11 07:38:02 +0800

    text gh-pages

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,6 @@
 
   <div class="p-4"> 
     <h1>You did it!</h1>
-    <p>
-      Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-      documentation
-    </p>
+
   </div>
 </template>
